f62603fd86ea20665644eb2bbcb69eea1133263e
Daniel Loos <dloos@bgc-jena.mpg.de>
2023-04-05 13:55:02 +0200

    Update build_tarballs.jl: Works on linux64

--- build_tarballs.jl/build_tarballs.jl
+++ build_tarballs.jl/build_tarballs.jl
@@ -7,37 +7,39 @@ version = v"0.1.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/danlooo/020-cxxcalc-binarybuilder.git", "b4021e03c83249b60b1dfffeb98ff752e5f56cad")
+    GitSource("https://github.com/danlooo/020-cxxcalc-binarybuilder.git", "bb08f182d3b8c0ead9f6ebe3f4c38b88fce7a343")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
+cd $WORKSPACE/srcdir/020-cxxcalc-binarybuilder/src
 export JlCxx_DIR=/workspace/$target/destdir/lib/cmake/JlCxx/
-cd 020-cxxcalc-binarybuilder/src
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release .
 make
-mkdir -p ${bindir}
-cp lib/* $bindir
+mkdir -p $libdir
+cp lib/* $libdir
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Platform("x86_64", "linux"; libc="glibc"),
-    Platform("aarch64", "linux"; libc="glibc")
+    Platform("x86_64", "linux"; libc="glibc")
+    # Platform("x86_64", "windows")
+    #Platform("aarch64", "linux"; libc="glibc")
 ]
 
+# Fix incompatibilities across GCC 4/5, because of std::string
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libCxxCalc", :cxxcalc)
+    LibraryProduct("libCxxCalc", :libCxxCalc)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="libjulia_jll", uuid="5ad3ddd2-0711-543a-b040-befd59781bbf"))
-    Dependency(PackageSpec(name="libcxxwrap_julia_jll", uuid="3eaa8342-bff7-56a5-9981-c04077f7cee7"))
+    BuildDependency(PackageSpec(name="libjulia_jll", uuid="5ad3ddd2-0711-543a-b040-befd59781bbf")),
+    BuildDependency(PackageSpec(name="libcxxwrap_julia_jll", uuid="3eaa8342-bff7-56a5-9981-c04077f7cee7"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
